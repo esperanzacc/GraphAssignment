b1e526f54d04e49f1ee83b7fe12fec7ba39af617
esperanzacc <v.esperanzac113@gmail.com>
2022-03-30 01:53:31 -0500

    Finish Graph Problems

--- ProblemSet2/CyClic Permutation/CyclicPermutation.swift
+++ ProblemSet2/CyClic Permutation/CyclicPermutation.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+// find cycle
+func cyclicPermutation() {
+  // input 3 2 7 8 1 4 5 6
+  // n = Test Cases
+  let n = Int(readLine()!)!
+  // go through each case, each case has num of items and sequence of Int
+  for _ in 0..<n {
+    let nums = Int(readLine()!)!
+    let firstLine = readLine()!.split(separator: " ")
+//    print(firstLine) -> ["3", "2", "7", "8", "1", "4", "5", "6"]
+    var count = 0
+    
+    var adjlist = [[Int]](repeating: [], count: nums + 1)
+    var visited = [Bool](repeating: false, count: nums + 1)
+    
+    for i in 0..<firstLine.count {
+      adjlist[i + 1].append(Int(firstLine[i])!)
+    }
+//    print(adjlist)
+    
+    for i in 1..<adjlist.count {
+      if !visited[i] {
+        count += dfs(start: i, adjlist: &adjlist, visited: &visited)
+      }
+    }
+    print(count)
+  }
+ 
+  func dfs(start: Int, adjlist: inout [[Int]], visited: inout [Bool]) -> Int {
+    visited[start] = true
+    // base case
+    if visited[adjlist[start][0]] {
+//      print([adjlist[start]])
+      return 1
+    }
+    // recursive case
+    return dfs(start: adjlist[start][0], adjlist: &adjlist , visited: &visited)
+  }
+  
+}
+//2
+//8
+//3 2 7 8 1 4 5 6
+//10
+//2 1 3 4 5 6 7 9 10 8
+
--- ProblemSet2/CyClic Permutation/RepeatingSquence.swift
+++ ProblemSet2/CyClic Permutation/RepeatingSquence.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+func repeatSequence() {
+  // if input 57, 2
+  let firstLine = readLine()!.split(separator: " ")
+  // first line = ["57", "2"]
+//  print("first: \(firstLine)")
+  // D[0] = a (1 <= a <= 9999)
+  let a = String(firstLine[0])
+  // a = "57"
+  // the power of p ( 1 <= p <= 5)
+  let p = String(firstLine[1])
+  // p = "2"
+  
+  var nums: [Int] = []
+ 
+  dfs(start: a)
+  
+  func dfs(start: String) {
+    // base case: contain the same Int will stop
+    if nums.contains(Int(start)!) {
+      print(nums.firstIndex(of: Int(start)!)!)
+//      print(start)
+      return
+    }
+    // if not stop, append start to the array -> means doesn't contain this value
+    nums.append(Int(start)!)
+    
+    // recursive case
+    var sum: Int = 0
+    for i in start {
+  //    print(Int(String(i)))
+      sum += (power(base: Int(String(i))!, exponent: Int(p)!))
+//      print(sum)
+    }
+    dfs(start: String(sum))
+  }
+//  print(nums)
+  
+  func power(base: Int, exponent: Int) -> Int {
+   // base case
+   if exponent == 0 {
+    return 1
+   }
+   // recursive case
+   return power(base: base, exponent: exponent - 1) * base
+  }
+}
